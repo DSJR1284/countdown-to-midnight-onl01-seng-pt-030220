c6019f8c2520709b9422d8964afebd86a6e3895e
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 02:32:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,11 @@ def countdown (counter)
 end
 
 
+def countdown_with_sleep (counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -=1
+    sleep (1 second)
+      end
+      return "HAPPY NEW YEAR!"
+end
